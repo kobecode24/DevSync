ed677dec76ae95f2b53f41b6e375e445eb87e585
kobe <kobecode24@outlook.com>
2024-10-14 21:28:48 +0100

    ADDED CronSchedule

--- src/main/java/org/devsyc/scheduler/QuartzInitializer.java
+++ src/main/java/org/devsyc/scheduler/QuartzInitializer.java
@@ -7,8 +7,13 @@ import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @WebListener
 public class QuartzInitializer implements ServletContextListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(QuartzInitializer.class);
 
     private Scheduler scheduler;
 
@@ -21,6 +26,20 @@ public class QuartzInitializer implements ServletContextListener {
             JobDetail dailyJob = JobBuilder.newJob(DailyTokenResetJob.class)
                     .withIdentity("dailyTokenReset", "tokenGroup")
                     .build();
+
+            /*Trigger dailyTrigger = TriggerBuilder.newTrigger()
+                    .withIdentity("dailyTokenResetTrigger", "tokenGroup")
+                    .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                            .withIntervalInSeconds(60)
+                            .repeatForever())
+                    .build();
+
+            Trigger monthlyTrigger = TriggerBuilder.newTrigger()
+                    .withIdentity("monthlyTokenResetTrigger", "tokenGroup")
+                    .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                            .withIntervalInSeconds(120)
+                            .repeatForever())
+                    .build();*/
 
             Trigger dailyTrigger = TriggerBuilder.newTrigger()
                     .withIdentity("dailyTokenResetTrigger", "tokenGroup")
@@ -40,6 +59,7 @@ public class QuartzInitializer implements ServletContextListener {
             scheduler.scheduleJob(monthlyJob, monthlyTrigger);
 
             scheduler.start();
+            logger.info("Quartz Scheduler started successfully");
         } catch (SchedulerException e) {
             e.printStackTrace();
         }
@@ -53,6 +73,7 @@ public class QuartzInitializer implements ServletContextListener {
             }
         } catch (SchedulerException e) {
             e.printStackTrace();
+            logger.error("Error starting Quartz Scheduler", e);
         }
     }
 }
